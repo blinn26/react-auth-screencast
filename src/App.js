3f8c2dd5a3a9e8aad849070c46a5044cdbaaba94
Kevin Loughead <kvnloughead@gmail.com>
2021-10-28 14:06:10 -0400

    add React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Profile from './components/Profile/Profile';
@@ -6,9 +8,17 @@ import Profile from './components/Profile/Profile';
 function App() {
   return (
     <div className="app">
-      <Profile />
-      <Register />
-      <Login />
+      <Switch>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/login">
+          <Login  />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
     </div>
   );
 }
