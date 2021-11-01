cc46c9819eaf27cbb07213d2f71b3cd2309c1c74
Kevin Loughead <kvnloughead@gmail.com>
2021-11-01 12:35:06 -0400

    screencast finished

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ isLoggedIn, children, path, ...props }) => {
   return (
-    <></>
+    <Route {...props}>
+      {isLoggedIn ? children : <Redirect to="/login" />}
+    </Route>
   );
 };
 
